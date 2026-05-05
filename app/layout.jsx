70181818e3295de8df0ed8df191bddb5cf07d7af
sadeedch <53071011+sadeedch@users.noreply.github.com>
2026-05-05 11:19:17 +1000

    Restore HTML structure in RootLayout component
    Uncommented the HTML structure in RootLayout.

--- app/layout.jsx
+++ app/layout.jsx
@@ -68,9 +68,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    // <html lang="en-AU">
-    //   <body>{children}</body>
-    // </html>
+  
 
       <html lang="en">
       <head>
